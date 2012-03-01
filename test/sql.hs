3922f49609a3c5f6f908dbbce788ab090efb8c8c
Dag Odenhall <dag.odenhall@gmail.com>
2012-03-01 16:57:53 +0100

    imaginary sql login

--- test/sql.hs
+++ test/sql.hs
@@ -8,3 +8,9 @@ hssqlppp =
    tablename = "my_table"
    varname   = "my_field"
    typename  = "text"
+
+login user password =
+    [sql| SELECT *
+            FROM users
+           WHERE name = $s(user)
+             AND password = $s(password) |]
