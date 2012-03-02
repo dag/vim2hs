c48cf1ce13d4dcc7167ec06771a1e63e2cfe96bf
Dag Odenhall <dag.odenhall@gmail.com>
2012-03-02 05:24:03 +0100

    test file for hsp

---
+++ test/hsp.hs
@@ -0,0 +1,20 @@
+simple =
+  <br/>
+
+simpleAttr =
+  <hr class="wide"/>
+
+block =
+  <p>Hello</p>
+
+blockAttr =
+  <a href=(Post 123)>Post #123</a>
+
+splice n =
+  <a href=(Post n)>Post #<% n %></a>
+
+list =
+  <%>
+    <p>Hello</p>
+    <p>World</p>
+  </%>
